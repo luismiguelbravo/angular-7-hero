e15e1fdbcf639d31a4449cf829050f9eb512b931
luismiguelbravo <lmbgott@gmail.com>
2019-04-18 17:37:01 -0300

    se agregan las rutas al router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HeroesComponent }      from './heroes/heroes.component';
+import { LoginComponent }       from './login/login.component';
+import { PanelDeBusquedaComponent }       from './panel-de-busqueda/panel-de-busqueda.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'heroes', component: HeroesComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'panel-de-busqueda', component: PanelDeBusquedaComponent },
+  { path: '', redirectTo: 'panel-de-busqeuda', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
